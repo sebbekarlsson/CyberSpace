81fcc7266e350c731d577cc9b59e54ca14354c88
Sebastian Karlsson <sebastian.karlsson@duva.se>
2015-05-13 18:10:22 +0200

    Minor update

--- src/cyber/main/entities/Cloud.java
+++ src/cyber/main/entities/Cloud.java
@@ -1,13 +1,17 @@
 package cyber.main.entities;
 
+import java.util.Random;
+
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector3f;
 
 import cyber.main.Entity;
 import cyber.main.Main;
 import cyber.main.TextureBank;
+import cyber.main.Util;
 
 public class Cloud extends Entity {
-
+	Random random = new Random();
 	public Cloud(float x, float y, float z) {
 		super(x, y, z);
 		sprite.textures.add(TextureBank.CLOUD);
@@ -17,6 +21,10 @@ public class Cloud extends Entity {
 	public void tick() {
 		if(z > Main.getCurrentScene().camera.z){
 			Main.getCurrentScene().destroy(this);
+		}
+		
+		if(random.nextInt(32) == 0){
+			addForce(new Vector3f(random.nextFloat()*Util.chooseInt(1, -1), random.nextFloat()*Util.chooseInt(1, -1), random.nextFloat()*Util.chooseInt(1, -1)));
 		}
 		
 	}
--- src/cyber/main/scenes/World.java
+++ src/cyber/main/scenes/World.java
@@ -23,9 +23,10 @@ public class World extends Scene {
 
 	@Override
 	public void tick() {
-		if(random.nextInt(10) == 0){
-			instantiate(new Cloud((camera.x+random.nextFloat()*64)*Util.chooseInt(1, -1), camera.y-7f, camera.z-64f));
-		}
+		//if(random.nextInt(3) == 0){
+			for(int i = 0; i < 6; i++)
+			instantiate(new Cloud((camera.x+random.nextFloat()*200)*Util.chooseInt(1, -1), (camera.y-32f)+random.nextFloat()*16, camera.z-128f));
+		//}
 		
 		camera.z = player.z+3f;
 		camera.y = player.y+2.5f;
